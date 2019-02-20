531d57134b28334d373dbf1a7b03e82da334ca1f
Justas Jaskunas <jaskunas.justas@gmail.com>
2019-02-20 17:12:36 +0000

    restaurants model,controller and bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,43 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get 'restaurants/new', to: 'restaurants#new',
+
+  # get 'restaurants', to: 'restaurants#index'
+
+  # get 'restaurants/:id', to: 'restaurants#show'
+  # post 'restaurants', to: 'restaurants#create'
+
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
+
+
+# resources :restaurants do
+#     collection do #restaurants
+#       get "top", to: "restaurants#top"
+#     end
+#     member do #restaurants/:id
+#       get "chef", to: "restaurants#chef"
+#     end
+#     resources :reviews, only: [:new, :create]
+#   end
+
+# get 'pets', to: 'pets#index'
+
+  # get 'pets/new', to: 'pets#new', as: 'new_pet'
+
+  # get 'pets/:id', to: 'pets#show', as: 'pet'
+
+  # post 'pets', to: 'pets#create'
+
+  # get 'pets/:id/edit', to: 'pets#edit', as: 'edit_pet'
+
+  # patch 'pets/:id', to: 'pets#update'
+
+  # delete 'pets/:id', to: 'pets#destroy'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+restaurants_attributes = [
+  {
+    name: 'Dishoom',
+    address: '7 Boundary St, London E2 7JE',
+    phone_number: '07400723465',
+    category: 'japanese'
+  },
+  {
+    name: 'Pizza East',
+    address: '56A Shoreditch High St, London E1 6PQ',
+    phone_number: '07400234333',
+    category: 'italian'
+  },
+  {
+    name: 'Five Guys',
+    address: '56A Trafalgar High St, London E1 6PQ',
+    phone_number: '07409239555',
+    category: 'chinese'
+  },
+  {
+    name: 'Pizza buddy',
+    address: '56A High St, Liverpool E1 6PQ',
+    phone_number: '07400234444',
+    category: 'belgian'
+  },
+  {
+    name: 'Steak house',
+    address: '36 Harn road, Peterborough Pe78gh',
+    phone_number: '07400230000',
+    category: 'french'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
